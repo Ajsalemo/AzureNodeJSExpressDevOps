cc0d01828633370d892ef21ce320afea9699af4f
Anthony Salemo <ajssalemo@gmail.com>
2020-12-09 17:34:12 -0500

    testing for validation

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express - Node.js - Azure DevOps Pipelines - Linux(prod)" });
+  res.render("index", { title: "Express - Node.js - Azure DevOps Pipelines" });
 });
 
 module.exports = router;
