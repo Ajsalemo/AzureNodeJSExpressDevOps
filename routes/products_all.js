96d9b52c46f5066f8a83b7b55b01934d908faa3a
Anthony Salemo <ajssalemo@gmail.com>
2021-02-24 16:44:39 -0500

    removed HttpOnly cookie header for testing

--- routes/products_all.js
+++ routes/products_all.js
@@ -17,7 +17,7 @@ const listOfProducts = [
 ];
 
 router.get("/", (req, res, next) => {
-  res.setHeader('Set-Cookie', 'foo=bar; HttpOnly');
+  res.setHeader('Set-Cookie', 'foo=bar');
   res.json(listOfProducts)
 });
 
