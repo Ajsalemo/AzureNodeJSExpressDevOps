3bb4f99091a9ebe1b269e1128289ddf3ff0c7532
Anthony Salemo <ajssalemo@gmail.com>
2021-03-23 11:16:24 -0400

    added nodejs cluster logic for shutdowns, exists or starts

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cluster = require("cluster");
+// Check the number of CPUs
+const numCPUs = require("os").cpus().length;
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const productsAllRouter = require("./routes/products_all");
 
 const app = express();
@@ -21,22 +23,47 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/api/products_all", productsAllRouter);
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// If the cluster module is available and the isMaster property exists, run the block
+if (cluster && cluster.isMaster) {
+  try {
+    console.log(`Master process ${process.pid} is running`);
+    // Fork a node process for each CPU available
+    for (let i = 0; i < numCPUs; i++) {
+      console.log(
+        `Creating a worker process for each CPU available - CPU ${i + 1}`
+      );
+      cluster.fork();
+    }
+    // On exit, log the worker PID that died
+    cluster.on("exit", (worker, code, signal) => {
+      console.log(`Worker process with PID ${worker.process.pid} died`);
+    });
+    // On disconnect, log the worker PID that died
+    cluster.on("disconnect", (worker, code, signal) => {
+      console.log(`Worker process with PID ${worker.process.pid} disconnected`);
+    });
+  } catch (error) {
+    console.log(
+      `An error has occurred during worker process creation - error: ${error}`
+    );
+    process.on("SIGTERM", () => {
+      console.info("SIGTERM signal received.");
+      console.log("Closing express server.");
+      process.exit(143);
+    });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
+    process.on("SIGKILL", () => {
+      console.info("SIGKILL signal received.");
+      console.log("Closing express server.");
+      process.exit(128);
+    });
+  }
+} else {
+  // Log the node process PID's and start express
+  console.log(`Worker process with PID ${process.pid} started`);
+  app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+}
 
 module.exports = app;
--- routes/products_all.js
+++ routes/products_all.js
@@ -1,23 +1,81 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 const listOfProducts = [
   {
-    id: 1,
-    product_name: "Eggs",
+    "_id": "5bd761dcae323e45a93cc771",
+    "name": "envelopes",
+    "tags": ["stationary", "office", "general"],
+    "price": "5.04",
+    "quantity": "8"
   },
   {
-    id: 2,
-    product_name: "Milk",
+    "_id": "5bd761dcae323e45a93abf2",
+    "name": "binder",
+    "tags": ["school", "general", "organization"],
+    "price": "11.61",
+    "quantity": "7"
   },
   {
-    id: 3,
-    product_name: "Bread",
+    "_id": "5bd761dcae323e45a9311231",
+    "name": "notepad",
+    "tags": ["office", "writing", "school"],
+    "price": "5.83",
+    "quantity": "5"
   },
+  {
+    "_id": "5bd761dcae323e45a9zwda1",
+    "name": "binder",
+    "tags": ["school", "general", "organization"],
+    "price": "18.72",
+    "quantity": "2"
+  },
+  {
+    "_id": "5bd761dcae323e45a9awdacw",
+    "name": "backpack",
+    "tags": ["school", "travel", "kids"],
+    "price": "116.93",
+    "quantity": "1"
+  },
+  {
+    "_id": {
+      "$oid": "604e715dcc0bd01140e60c22"
+    },
+    "name": "staples",
+    "tags": ["writing", "office", "school"],
+    "quantity": "200",
+    "price": "1.19",
+  },
+  {
+    "_id": {
+      "$oid": "604e71a99ad46e4b45ca7c55"
+    },
+    "name": "staples",
+    "tags": ["writing", "office", "school"],
+    "quantity": "200",
+    "price": "1.19",
+  },
+  {
+    "_id": {
+      "$oid": "604e7a1fcff8422010dde3d1"
+    },
+    "name": "envelopes",
+    "tags": ["stationary", "office", "general"],
+    "price": "6.04",
+    "quantity": "20"
+  },
+  {
+    "_id": {
+      "$oid": "604fcdaf6ad66611a575a8f7"
+    },
+    "name": "staples",
+    "tags": ["writing", "office", "school"],
+    "quantity": "100",
+    "price": "0.89",
+  }
 ];
 
 router.get("/", (req, res, next) => {
-  res.setHeader('Set-Cookie', 'foo=bar');
   res.json(listOfProducts)
 });
 
